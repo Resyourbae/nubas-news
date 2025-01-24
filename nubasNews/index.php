7f65183e209d658523d53f073a7bb357c955c1fa
ayserNii <147005714+Resyourbae@users.noreply.github.com>
2025-01-24 09:44:19 +0700

    Add files via upload

---
+++ nubasNews/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="login-box">
+        <div class="login-header">
+            <header>Login</header>
+        </div>
+        <div class="input-box">
+            <input type="text" class="input-field" placeholder="Username" autocomplete="off" required>
+        </div>
+        <div class="input-box">
+            <input type="email" class="input-field" placeholder="Email" autocomplete="off" required>
+        </div>
+        <div class="input-box">
+            <input type="password" class="input-field" placeholder="Password" autocomplete="off" required>
+        </div>
+        <div class="forgot">
+            <section>
+                <input type="checkbox" id="check">
+                <label for="check">Ingat saya</label>
+            </section>
+            <section>
+                <a href="#">Lupa Password?</a>
+            </section>
+        </div>
+        <div class="input-submit">
+            <button class="input-btn">Login</button>
+        </div>
+        <div class="sign-up-link">
+            <p>Belum Memiliki Akun? <a href="#">Daftar Sekarang</a></p>
+        </div>
+    </div>
+</body>
+</html>
